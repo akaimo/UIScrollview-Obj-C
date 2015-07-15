72dbd2f4f1ac33d17f482440cbbf1e014fb6a7a3
shuhei <kawashu0723@gmail.com>
2015-07-15 18:45:39 +0900

    Initial Commit

---
+++ UIScrollview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIScrollview
+//
+//  Created by akaimo on 2015/07/15.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIScrollview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScrollview
+//
+//  Created by akaimo on 2015/07/15.
+//  Copyright (c) 2015年 akaimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
